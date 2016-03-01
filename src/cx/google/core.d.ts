aaeca6f4e35027721bc5d73a9e53b9d15897f22d
Fredrik Bonander <carl.fredrik.bonander@gmail.com>
2016-03-01 15:16:29 +0000

    chore: goldfish

---
+++ src/cx/google/core.d.ts
@@ -0,0 +1,22 @@
+declare module 'cx/google/core' {
+  class Client {
+    static _script_loaded: boolean;
+    static _api_load_promise: Promise<any>;
+    static bootstrap(preload_api?: string): Promise<any>;
+    static load(name: string, version: string): Promise<{}>;
+    static _add_script_tag(resolve: any, reject: any, preload_api: string): void;
+    static _api_has_loaded(resolve: any, reject: any, preload_api: string): void;
+  }
+
+  class Auth {
+    scopes: Array<string>;
+    clientId: string;
+    private _googleAuth;
+    private _hook;
+    constructor(clientId: string, scopes?: Array<string>);
+    authorize(): Promise<{}>;
+    checkAuth(): Promise<{}>;
+    signOut(): Promise<{}>;
+    private _wait(fn);
+  }
+}
